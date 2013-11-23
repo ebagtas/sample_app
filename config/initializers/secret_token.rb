29a676f095a6f05d9cf78f167c1cd1333483fa85
Eric Bagtas <eric.bagtas@gmail.com>
2013-11-23 08:28:59 -0500

    Make a basic User model (including secure passwords)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCms::Application.config.secret_token = 'fe6f103d5fe657687573db3a34d733a79014485b8d6b84c58c2709f2e58c89453ecda69aa0190e6851b7755f5bfb046b9a6d63a800fd1a1fca5644d920c23e9b'
